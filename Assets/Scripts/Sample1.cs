153ac79d9deff8e9fd8d3ea003ba790ecb556f00
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-12 05:35:42 +0900

    UniTaskでも動かした

--- Assets/Scripts/Sample1.cs
+++ Assets/Scripts/Sample1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Threading;
 using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 
 public class Sample1 : MonoBehaviour
 {
@@ -12,14 +13,26 @@ public class Sample1 : MonoBehaviour
 
         var Result = await Work();
         Debug.Log(Result);
+
+        Result = await Work2();
+        Debug.Log(Result);
     }
 
-    async Task<string> Work()
+    private async Task<string> Work()
     {
         return await Task.Run(() =>
         {
             Debug.Log("Working Thread:" + Thread.CurrentThread.ManagedThreadId);
             return "Work Finish Thread:" + Thread.CurrentThread.ManagedThreadId;
+        });
+    }
+
+    private async UniTask<string> Work2()
+    {
+        return await Task.Run(() =>
+        {
+            Debug.Log("Working UniTask Thread:" + Thread.CurrentThread.ManagedThreadId);
+            return "Work Finish UniTask Thread:" + Thread.CurrentThread.ManagedThreadId;
         });
     }
 }
