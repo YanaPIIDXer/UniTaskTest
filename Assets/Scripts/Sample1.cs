ffc3ed35551049f6390ac4e59eaa60154723fb5a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-12 05:30:10 +0900

    とりあえず普通のTask

---
+++ Assets/Scripts/Sample1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class Sample1 : MonoBehaviour
+{
+    async void Start()
+    {
+        Debug.Log("Start Thread:" + Thread.CurrentThread.ManagedThreadId);
+
+        var Result = await Work();
+        Debug.Log(Result);
+    }
+
+    async Task<string> Work()
+    {
+        return await Task.Run(() =>
+        {
+            Debug.Log("Working Thread:" + Thread.CurrentThread.ManagedThreadId);
+            return "Work Finish Thread:" + Thread.CurrentThread.ManagedThreadId;
+        });
+    }
+}
