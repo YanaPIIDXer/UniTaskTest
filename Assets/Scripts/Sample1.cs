8d344af3863e6071057cde4468c0e75b0a72f2bc
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-02 13:53:10 +0900

    CancellationTokenを渡すようにした

--- Assets/Scripts/Sample1.cs
+++ Assets/Scripts/Sample1.cs
@@ -11,30 +11,32 @@ public class Sample1 : MonoBehaviour
     {
         Debug.Log("Start Thread:" + Thread.CurrentThread.ManagedThreadId);
 
-        var Result = await Work();
+        var Token = this.GetCancellationTokenOnDestroy();
+
+        var Result = await Work(Token);
         Debug.Log(Result);
 
-        Result = await Work2();
+        Result = await Work2(Token);
         Debug.Log(Result);
     }
 
-    private async Task<string> Work()
+    private async Task<string> Work(CancellationToken Token)
     {
         return await Task.Run(async () =>
         {
             Debug.Log("Working Thread:" + Thread.CurrentThread.ManagedThreadId);
-            await Task.Delay(1000);
+            await Task.Delay(1000, Token);
             return "Work Finish Thread:" + Thread.CurrentThread.ManagedThreadId;
-        });
+        }, Token);
     }
 
-    private async UniTask<string> Work2()
+    private async UniTask<string> Work2(CancellationToken Token)
     {
         return await Task.Run(async () =>
         {
             Debug.Log("Working UniTask Thread:" + Thread.CurrentThread.ManagedThreadId);
-            await Task.Delay(1000);
+            await Task.Delay(1000, Token);
             return "Work Finish UniTask Thread:" + Thread.CurrentThread.ManagedThreadId;
-        });
+        }, Token);
     }
 }
